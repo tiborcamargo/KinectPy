82012a71682cc479b4290fd1741058ef77795d40
Tibor Camargo <tibor.zequini@gmail.com>
2022-08-17 16:44:47 +0200

    added argument to load less frames, for faster visualization

--- visualize.py
+++ visualize.py
@@ -10,16 +10,18 @@ from utils.processing import sort_filenames_by_timestamp
 parser = ArgumentParser()
 parser.add_argument("-d", "--dir", help="root directory", type=str)
 parser.add_argument("-s", "--skeleton", help="display skeleton", action='store_true')
+parser.add_argument("-n", "--n_frames", help="number of loaded point clouds", type=int, default=100)
 args = parser.parse_args()
 
 
 if __name__ == '__main__':
     
+    print(f'Displaying only {args.n_frames} frames.')
     root_dir = args.dir
     pcd_dir = os.path.join(root_dir, 'master_1/filtered_and_registered_pointclouds')
     skeleton_fp = os.path.join(root_dir, 'master_1/skeleton/registered_positions_3d.csv')
 
-    pcd_filenames = sort_filenames_by_timestamp(os.listdir(pcd_dir))
+    pcd_filenames = sort_filenames_by_timestamp(os.listdir(pcd_dir))[:args.n_frames]
     pcds = [o3d.io.read_point_cloud(os.path.join(pcd_dir, fn)) for fn in pcd_filenames]
 
     viewer = MoCapViewer(grid_axis=None)
