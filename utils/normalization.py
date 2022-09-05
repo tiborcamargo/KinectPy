e0bce63162fd309ffc0b5fd295211f0f621c0b6a
Tibor Camargo <tibor.zequini@gmail.com>
2022-09-05 22:01:31 +0200

    added translation normalization

--- utils/normalization.py
+++ utils/normalization.py
@@ -45,7 +45,44 @@ def obb_normalization_batch(x, y):
     return obb_points, obb_joints
 
 
+def translation_normalization_batch(x, y):
+    obb_points = []
+    obb_joints = []
+
+    for batch_idx in range(x.shape[0]):
+        points = x[batch_idx]
+        joints = y[batch_idx]
+        
+        # Create temp pcd to retrieve OBB translation + rotation
+        tmp_pcd = o3d.geometry.PointCloud(points=o3d.utility.Vector3dVector(points))
+
+        # Retrieve rotation and translation
+        obb = tmp_pcd.get_oriented_bounding_box()
+        obb_trans = obb.get_center()
+
+        # Normalize points
+        obb_normalized_points = (points - obb_trans)
+        obb_points.append(obb_normalized_points)
+
+        # Normalize joints
+        joints_3d = joints.reshape((joints.shape[0]//3, 3))
+        obb_normalized_joints = (joints_3d - obb_trans)
+        obb_normalized_joints = obb_normalized_joints.reshape((joints.shape[0]))
+        obb_joints.append(obb_normalized_joints)
+
+    obb_points = np.array(obb_points)
+    obb_joints = np.array(obb_joints)
+    
+    return obb_points, obb_joints
+
+
 @tf.function(input_signature=[tf.TensorSpec(shape=(None, None, 3)), tf.TensorSpec(shape=(None, None))])
 def normalize_obb(x, y):
     x, y = tf.numpy_function(obb_normalization_batch, [x, y], (tf.double, tf.double))
+    return x, y
+
+
+@tf.function(input_signature=[tf.TensorSpec(shape=(None, None, 3)), tf.TensorSpec(shape=(None, None))])
+def translate(x, y):
+    x, y = tf.numpy_function(translation_normalization_batch, [x, y], (tf.double, tf.double))
     return x, y
