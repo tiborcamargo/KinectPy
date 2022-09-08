c04cb0abdb6f57cc2dd14cbecba54b68a6a1144d
Tibor Camargo <tibor.zequini@gmail.com>
2022-09-08 23:31:29 +0200

    added unique metric name to each threshold

--- metrics/metric.py
+++ metrics/metric.py
@@ -1,13 +1,16 @@
 import numpy as np
 import tensorflow as tf
 from typing import Union
+from functools import wraps
 
 
 def percentual_correct_keypoints(threshold=150):
+    @wraps(percentual_correct_keypoints)
     def PercentualCorrectKeypoints(y_true, y_pred):
         y_true = tf.cast(y_true, tf.float32)
         y_pred = tf.cast(y_pred, tf.float32)
         numerator = tf.where(tf.abs(y_pred - y_true) <= threshold, x=1.0, y=0.0)
         value = tf.math.reduce_mean(numerator)
         return value
+    PercentualCorrectKeypoints.__name__ = f'percentual_correct_keypoints_{str(threshold)}'
     return PercentualCorrectKeypoints
