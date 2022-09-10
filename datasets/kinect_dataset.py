69ff0cc006e8f9a394417c2881a7282199dcd2a3
Tibor Camargo <tibor.zequini@gmail.com>
2022-09-10 23:55:15 +0200

    changed implementation for dataset to allow for faster loader

--- datasets/kinect_dataset.py
+++ datasets/kinect_dataset.py
@@ -1,27 +1,29 @@
 import os
-import glob
+import random
 import open3d as o3d
 import pandas as pd
 import tensorflow as tf
 import numpy as np
-from typing import List, Tuple, Literal
+from typing import List, Literal
+import gc
+gc.disable()
 np.set_printoptions(suppress=True)
 
 
 class KinectDataset:
     def __init__(
         self,
-        master_root_dirs: List[str],
+        subjects_dirs, 
         joints: List[str],
         number_of_points: int,
         flag: Literal['train', 'val', 'test']
         ):
         """
         Args:
-            master_root_dirs: Path to the master's device root dir, eg: /path/to/master_1/
-            batch_size: 
+            subjects_dirs: ['/path/to/train/subject1/', '/path/to/train/subject2', ...]
+            batch_size: Number of points in a batch
             joints: Skeleton joints defined by your RGB-D sensor, such as: ['PELVIS', 'FOOT', ...],
-                    see all options in the 
+                    see all options in options folder
             number_of_points: Number of points to be sub-sampled in a point cloud
             flag: whether the dataset is used for training, validation or testing,
                   it is used to enforce a directory structure such as 
@@ -37,31 +39,25 @@ class KinectDataset:
         self.flag = flag
         self.joints_columns = np.concatenate([[joint + ' (x)', joint + ' (y)', joint + ' (z)']  for joint in joints])
 
-        # Verify if flag corresponds to the directory structure being passed
-        pcd_dirs = []
+        master_root_dirs = []
+        for subject in subjects_dirs:
+            for experiment in os.listdir(subject):
+                if experiment.find('_') < 0:
+                    master_dir = os.path.join(subject, experiment, 'master_1')
+                    master_root_dirs.append(master_dir)
+                    
         self.pointcloud_files = []
-        for master_root_dir in master_root_dirs:
-
-            # asserting if train/val/test is passed correctly
-            if flag not in os.path.abspath(master_root_dir).split(os.path.sep):
-                 raise Exception('Directory structure does not match the dataset flag')
-
-            # Point cloud directories and its corresponding skeleton CSV files
-            pcd_dir_regex = os.path.abspath(os.path.join(master_root_dir, 'filtered_and_registered_pointclouds'))
-            pcd_dirs.append(glob.glob(pcd_dir_regex))
-            self.pointcloud_files.append(glob.glob(os.path.join(pcd_dir_regex, '*.pcd')))
-
-        # transforming lists to numpy array and shuffling pcd filepaths
-        pcd_dirs = np.concatenate(pcd_dirs)
-        self.pointcloud_files = np.concatenate(self.pointcloud_files)
-        self.dataset_size = len(self.pointcloud_files)
-        np.random.shuffle(self.pointcloud_files)
-
-        # Mapping filename to a skeleton CSV
         self.correspondent_skeleton_csv = {}
-        for pcd_dir in pcd_dirs:
+
+        for master_root_dir in master_root_dirs:
+            pcd_dir = os.path.join(master_root_dir, 'filtered_and_registered_pointclouds')
+            self.pointcloud_files.extend([os.path.join(pcd_dir, fn) for fn in os.listdir(pcd_dir)])
+
             skeleton_fp = pcd_dir.replace('filtered_and_registered_pointclouds', os.path.join('skeleton', 'synced_positions_3d.csv'))
             self.correspondent_skeleton_csv[pcd_dir] = pd.read_csv(skeleton_fp, index_col='timestamp')
+
+        random.shuffle(self.pointcloud_files)
+        self.dataset_size = len(self.pointcloud_files)
 
         # Creating tensorflow dataset
         self.tf_dataset = tf.data.Dataset.from_generator(
@@ -84,25 +80,30 @@ class KinectDataset:
         """
         pointcloud_files = [file.decode('utf-8') for file in pointcloud_files]
         i = 0 
-        for file in pointcloud_files:
+        for file in list(pointcloud_files):
+            # The try-exception is here for when pcds cant find a correspondence in csv
+            try:
+                # Finding the proper skeleton dataframe and the proper timestamp 
+                timestamp = int(file.split(os.path.sep)[-1][:-4])
 
-            # Finding the proper skeleton dataframe and the proper timestamp 
-            timestamp = int(file.split(os.path.sep)[-1][:-4])
-            correspondent_skeleton_key = tf.io.gfile.join(
-                os.path.sep.join(file.split(os.path.sep)[:-2]), 
-                'filtered_and_registered_pointclouds'
-            )
+                correspondent_skeleton_key = tf.io.gfile.join(
+                    os.path.sep.join(file.split(os.path.sep)[:-2]), 
+                    'filtered_and_registered_pointclouds'
+                )
 
-            # Retrieving skeletons positions and joints
-            skeleton_df = self.correspondent_skeleton_csv[correspondent_skeleton_key]
-            skeleton_positions = skeleton_df[self.joints_columns].loc[timestamp].values
+                # Retrieving skeletons positions and joints
+                skeleton_df = self.correspondent_skeleton_csv[correspondent_skeleton_key]
+                skeleton_positions = skeleton_df[self.joints_columns].loc[timestamp].values
 
-            ## Reading point cloud and sampling
-            pcd = o3d.io.read_point_cloud(file)
-            pcd_points = self._select_points_randomly(pcd, number_of_points)
-                
-            yield pcd_points, skeleton_positions
-            i = i + 1
+                ## Reading point cloud and sampling
+                pcd = o3d.io.read_point_cloud(file)
+                pcd_points = self._select_points_randomly(pcd, number_of_points)
+
+                yield pcd_points, skeleton_positions
+                i = i + 1
+
+            except Exception as e:
+                print(e, flush=True)
                 
                 
     def __call__(self):
