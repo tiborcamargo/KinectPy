b05aaddc2875b981e254e349a7cf2ac00641fe7f
Tibor Camargo <tibor.zequini@gmail.com>
2022-09-11 18:10:33 +0200

    giving index to joint

--- options/joints.py
+++ options/joints.py
@@ -6,3 +6,18 @@ POSSIBLE_JOINTS = ['PELVIS', 'SPINE_NAVAL', 'SPINE_CHEST', 'NECK',
                    'HIP_LEFT', 'KNEE_LEFT','ANKLE_LEFT','FOOT_LEFT','HIP_RIGHT',
                    'KNEE_RIGHT','ANKLE_RIGHT','FOOT_RIGHT','HEAD', 'NOSE', 
                    'EYE_LEFT','EAR_LEFT','EYE_RIGHT', 'EAR_RIGHT']
+
+
+JOINTS_INDICES = {'PELVIS': 0, 'SPINE_NAVAL': 1, 'SPINE_CHEST': 2, 'NECK': 3, 
+                  'CLAVICLE_LEFT': 4, 'SHOULDER_LEFT': 5, 'ELBOW_LEFT': 6, 
+                  'WRIST_LEFT': 7, 'HAND_LEFT': 8, 'HANDTIP_LEFT': 9, 'THUMB_LEFT': 10,
+                  'CLAVICLE_RIGHT': 11, 'SHOULDER_RIGHT': 12, 'ELBOW_RIGHT': 13, 'WRIST_RIGHT': 14, 
+                  'HAND_RIGHT': 15, 'HANDTIP_RIGHT': 16, 'THUMB_RIGHT': 17, 'HIP_LEFT': 18,
+                  'KNEE_LEFT': 19, 'ANKLE_LEFT': 20, 'FOOT_LEFT': 21, 'HIP_RIGHT': 22, 'KNEE_RIGHT': 23,
+                  'ANKLE_RIGHT': 24, 'FOOT_RIGHT': 25, 'HEAD': 26, 'NOSE': 27, 'EYE_LEFT': 28, 'EAR_LEFT': 29,
+                  'EYE_RIGHT': 30, 'EAR_RIGHT': 31}
+
+
+LOWER_JOINTS = ['PELVIS', 'HIP_LEFT', 'HIP_RIGHT',
+                'KNEE_LEFT', 'KNEE_RIGHT', 
+                'ANKLE_LEFT', 'ANKLE_RIGHT']
