f832755351c2fb215738c02172cdf42b96bd9509
Tibor Camargo <tibor.zequini@gmail.com>
2022-10-03 22:15:53 +0200

    added new function to visualize colored pointclouds with multiple joints

--- utils/visualization.py
+++ utils/visualization.py
@@ -2,7 +2,7 @@ import open3d as o3d
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from typing import Union
+from typing import Union, List
 from PyMoCapViewer import MoCapViewer
 
 
@@ -45,4 +45,44 @@ def visualize_predictions(
         skeleton_pred = pd.DataFrame(y_pred, columns=columns)
         viewer.add_skeleton(skeleton_pred, color='red')
         
+    viewer.show_window()
+
+
+def display_pcd_and_joints(
+    pointclouds: Union[None, List[o3d.geometry.PointCloud]] = None, 
+    joints: Union[None, np.ndarray] = None,
+    joints_columns: List[str] = ['PELVIS']
+    ) -> None:
+    '''
+    Args:
+        joints: array of joints for one or multiple devices, 
+                it accepts the following shape:
+                joints.shape = (num_of_frames, num_of_devices, num_of_joints*3)
+                where `num_of_device` expects at least one device (master_1 or sub_1/sub_2/...)
+    
+    '''
+
+    print('TODO: Implement joint names')
+    
+    viewer = MoCapViewer(grid_axis=None)
+    if pointclouds is not None:
+        viewer.add_point_cloud_animation(pointclouds)
+
+    if joints is not None:
+        num_frames, num_devices, cols = np.shape(joints)
+        for i in range(num_devices):
+            if i == 0:
+                c = 'red'
+            elif i == 1:
+                c = 'green'
+            elif i == 2:
+                c = 'blue'
+            else:
+                c = 'gray'
+            viewer.add_skeleton(
+                pd.DataFrame(joints[:, i, :], columns=joints_columns), 
+                skeleton_connection='azure', 
+                color=c
+                )
+
     viewer.show_window()
