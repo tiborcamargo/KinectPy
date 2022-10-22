53726b83535bdb17cc2bda6624d444c2b2dd254e
Tibor <tibor.zequini@gmail.com>
2022-10-22 14:55:59 +0200

    created skeleton fusion based on gradient and centroid

--- utils/skeleton_fusion.py
+++ utils/skeleton_fusion.py
@@ -3,215 +3,129 @@ import copy
 import open3d as o3d
 import numpy as np
 import pandas as pd
+from typing import List
+import sys
+import matplotlib.pyplot as plt
 
-from typing import List, Union
+plt.style.use('ggplot')
+np.set_printoptions(suppress=True)
+sys.path.append('..')
+
 from utils.processing import synchronize_filenames, synchronize_joints, select_points_randomly, distance_between_joints
 from utils.visualization import visualize_skeletons_2d
+from utils.skeleton_fusion import (
+    compute_centroid, compute_threshold, distance_joints_to_centroid,
+    distance_all_joints_to_central_joint, normalized_weight_of_frame
+)
+
 from PyMoCapViewer import MoCapViewer
 from itertools import combinations
 
-
-def compute_threshold(
-    skeleton_dataframes: List[pd.DataFrame],
-    confidence_dataframes: List[pd.DataFrame]
-    ) -> float:
-    '''
-    (Equation 12)
-    '''
-    num_frames = len(skeleton_dataframes[0])
-    sum_of_dists = 0
-    
-    for frame in range(num_frames):
-        sum_of_dists += average_distance_between_joints(
-            skeleton_dataframes, 
-            confidence_dataframes, 
-            frame
-        )
-        
-    alpha = (1/num_frames)*sum_of_dists
-    return alpha
+import logging
+logging.disable(logging.CRITICAL)
 
 
-def normalized_weight_of_frame(
-    confidence_dataframes: List[pd.DataFrame], 
-    valid_cameras: List[int], 
-    camera_idx: int,
-    frame: int
-    ) -> float:
-    ''' 
-    (Equation 9)
-    
-    Normalized weight of frame F_ct over ̈C_i^{..}
-    '''
-    sum_of_weights = 0
-    
-    for c in valid_cameras:
-        sum_of_weights += weight_of_frame(confidence_dataframes[c], frame)
-        
-    return weight_of_frame(confidence_dataframes[camera_idx], frame)/sum_of_weights
-
-
-def average_distance_between_joints(
-    skeleton_dataframes: List[pd.DataFrame], 
-    confidence_dataframes: List[pd.DataFrame], 
-    frame: int
-    ) -> float:
-    '''
-    (Equation 10)
-    '''
-    
-    average_distance_between_joints = 0
-    joints_tracked = joints_tracked_by_all_cameras(confidence_dataframes, frame)
-    
-    for joint in joints_tracked:
-        average_distance_between_joints += distance_between_joints(skeleton_dataframes, frame, joint)
-
-    average_distance_between_joints = average_distance_between_joints/len(joints_tracked)
-    return average_distance_between_joints
-
-
-def joints_tracked_by_all_cameras(
-    confidence_dataframes: List[pd.DataFrame], 
-    frame: int
-    ) -> List[int]:
-    '''
-    Given C = {C1, ..., Cm} cameras, return which joints are being 
-    CONFIDENTLY tracked by ALL cameras in an specific frame.
-    
-    For example if joints 0, 1, 2 and 5 are being tracked by all
-    Cm cameras, then it returns:
-         joints_tracked_all_cameras = [0, 1, 2, 5]
-    '''
-    number_of_cameras = len(confidence_dataframes)
-    joints_tracked = []
-    
-    joints_tracked = []
-    for c in range(3):
-        tracked_joints_mask = confidence_dataframes[c].iloc[frame] == 2
-        tracked = tracked_joints_mask[tracked_joints_mask == True].index
-        tracked = np.nonzero(tracked)[0].tolist()
-        joints_tracked.append(tracked)
-
-    joints_tracked_all_cameras = list(set.intersection(*map(set, joints_tracked)))
-    
-    return joints_tracked_all_cameras
-
-
-def weight_of_frame(confidence_dataframe: pd.DataFrame, frame: int):
-    '''
-    (Equation 2)
-    '''
-    return np.sum(confidence_dataframe.iloc[frame])
-
-
-def distance_between_joints(
-    skeleton_dataframes: List[pd.DataFrame], 
-    frame: int, 
-    joint: int
-    ) -> float:
-    ''' 
-    (Equation 11)
-
-    Compute the distance between all correspondent joints `joint` and 
-    normalize by the number of possible camera combinations. 
-    
-    For example: 
-        joint = 0 ('KNEE_RIGHT')
-            dist(knee_right_camera_0, knee_right_camera_1) = x01
-            dist(knee_right_camera_0, knee_right_camera_2) = x02
-            dist(knee_right_camera_1, knee_right_camera_2) = x12
-        return
-            total_dist: (x01 + x02 + x12)/C(3, 2)
-    '''
-    if joint > len(skeleton_dataframes[0].columns)//3 - 1:
-        raise ValueError('Not enough joints in the dataframe')
-        
-    m_cameras = len(skeleton_dataframes)
-    camera_combinations = list(combinations(range(m_cameras), 2))
-    num_combinations = len(camera_combinations)
-    
-    total_dist = 0
-    for c1, c2 in camera_combinations:
-        total_dist += np.linalg.norm(
-            skeleton_dataframes[c1].iloc[frame][joint*3:(joint+1)*3] - skeleton_dataframes[c2].iloc[frame][joint*3:(joint+1)*3]
-        )
-    total_dist = total_dist/num_combinations
-    return total_dist
-
-
-def compute_centroid(
-    joints_per_camera: List[np.ndarray], 
-    frame: int,
-    joint: int
+def fuse_skeletons_gradient(
+    skeletons: List[np.ndarray],
+    alpha: float = 1.4,
+    beta: float = 1.4
     ) -> np.ndarray:
     '''
-    Compute centroid for the joint `joint` seen by multiple cameras
-    Assumes that each element in `joints_per_camera` is reshaped
-    in the shape (num of frames, num of joints, 3)
-    
-    Args:
-      joints_per_camera: 
-        List containing the joints obtained for each camera
-        with the following array shape:
-        joints_per_camera[i] : (number of frames, number of joints, 3)
-                
-    Returns:
-      centroids:
-        centroid for all the joints in frame `frame`,
-        with the following shape:
-        centroids = (number of joints, 3)
+    Apply skeleton fusion using a gradient-based weighted average, 
+    along with centroid-based distances
+
+    Let p1,...,pK be a joint for camera k=1,..,K
+    The fused skeleton p* is given by
+
+    p* = (w1*p1 + w2*p2 + ... + wK*pK)/(w1+...+wK)
+    where wi = 1/||pi(t) - pi(t-1)||^alpha * 1/||pi(t) - c(t)||^beta
+
+    with c(t) being the centroid of joints p1,...,pK at time t
+
+    if alpha != 0, it becomes a gradient-based fusion
+    if beta != 0, it becomes  centroid-based fusion
+    if alpha = beta = 0, then it becomes a simple averaging
     '''
-    number_of_cameras = len(joints_per_camera)
-    theta_i = np.zeros_like(joints_per_camera[0][frame][joint])
-    for c in range(number_of_cameras):
-        theta_i += joints_per_camera[c][frame][joint]
+    # TODO: Incorporate m cameras instead of fixing 3
+    num_cameras, num_frames, num_joints, num_dims = skeletons.shape
+    initial_frame = 20
+    fused_joints = np.zeros_like(skeletons[0])
+    fused_joints[:initial_frame, :, :] = np.mean(skeletons[:, :initial_frame, :, :], axis=0)
+
+    grads1 = []
+    grads2 = []
+    grads3 = []
+
+    for f in range(initial_frame, num_frames):
+        for j in range(num_joints):
+            
+            last_point = fused_joints[f - 1, j]
+
+            p1 = skeletons[0, f, j]
+            p2 = skeletons[1, f, j]
+            p3 = skeletons[2, f, j]
+            centroid = (p1 + p2 + p3)/3
+
+            # Distance with respect to last averaged point
+            grad1 = np.linalg.norm(p1 - last_point)
+            grad2 = np.linalg.norm(p2 - last_point)
+            grad3 = np.linalg.norm(p3 - last_point)
+
+            w1 = 1.0 / ((grad1 ** alpha)*(np.linalg.norm(p1 - centroid) ** beta))
+            w2 = 1.0 / ((grad2 ** alpha)*(np.linalg.norm(p2 - centroid) ** beta))
+            w3 = 1.0 / ((grad3 ** alpha)*(np.linalg.norm(p3 - centroid) ** beta))
+
+            weighted_average = (w1 * p1 + w2 * p2 + w3 * p3)/(w1 + w2 + w3)
+            fused_joints[f, j] = weighted_average
+
+    return fused_joints, grads1, grads2, grads3
+
+
+if __name__ == '__main__':
+
+    master_root_dir = 'C:/Users/Tibor/Documents/Kinect_Data/patient_11/master_1/'
+    sub_1_root_dir = 'C:/Users/Tibor/Documents/Kinect_Data/patient_11/sub_1/'
+    sub_2_root_dir = 'C:/Users/Tibor/Documents/Kinect_Data/patient_11/sub_2/'
+    root_dirs = [master_root_dir, sub_1_root_dir, sub_2_root_dir]
+    trafos = [np.load(os.path.join(root_dirs[0], f'transformation_master_sub_{i}.npy')) for i in range(1, len(root_dirs))]
+    joint_names = ['SPINE_NAVEL', 'SPINE_CHEST', 'PELVIS', 'HIP_LEFT', 'KNEE_LEFT','ANKLE_LEFT','FOOT_LEFT','HIP_RIGHT','KNEE_RIGHT','ANKLE_RIGHT','FOOT_RIGHT']
+
+    device_idx = 0
+    skeleton_dfs, confidence_dfs = synchronize_joints(root_dirs, trafos, joint_names, True)
+    pred_dfs = copy.deepcopy(skeleton_dfs[device_idx])
+    pcd_filenames = synchronize_filenames(root_dirs).dropna().astype(int).iloc[1:]
+
+    reshaped_skeletons = np.array([skeleton_dfs[i].values.reshape(skeleton_dfs[i].shape[0], -1, 3) for i in range(len(skeleton_dfs))])
+    fused_skel, grads1, grads2, grads3 = fuse_skeletons_gradient(reshaped_skeletons, 1.4)
+    fused_skel = fused_skel.reshape(fused_skel.shape[0], fused_skel.shape[1]*fused_skel.shape[2])
+    fused_df = pd.DataFrame(fused_skel, columns=skeleton_dfs[0].columns)
+
+    for i in range(len(grads1)):
+        print(f'grads1: {grads1[i]}, grads2: {grads2[i]}, grads3: {grads3[i]} \n')
+    start_f = 100
+    end_f = 500
+
+    load_pcds = True
+
+    if load_pcds:
+        pcds = []
+
+        for timestamp in pcd_filenames['master_1'].iloc[start_f:end_f+1]:
+            master_pcd_fn = os.path.join(master_root_dir, 'filtered_and_registered_pointclouds', str(timestamp) + '.pcd')
+            master_pcd = o3d.io.read_point_cloud(master_pcd_fn)
+
+            pcd = copy.deepcopy(master_pcd)
+            # points = select_points_randomly(master_pcd, 4096//4)
+            # pcd = o3d.geometry.PointCloud()
+            # pcd.points = o3d.utility.Vector3dVector(points)
+            pcds.append(pcd)
+            
         
-    centroids = theta_i/number_of_cameras
-    return centroids
-
-
-def distance_joints_to_centroid(
-    joints_per_camera: List[np.ndarray],
-    centroids: np.ndarray,
-    camera_idx: int,
-    frame: int,
-    joint: int
-    ) -> np.ndarray:
-    ''' 
-    Compute the distance of a single joint seen by all camera `camera_idx` 
-    and the centroid
-    '''
-    return np.linalg.norm(joints_per_camera[camera_idx][frame][joint] - centroids) 
-
-
-def find_center_joint(
-    joints_per_camera: List[np.ndarray],
-    centroids: np.ndarray,
-    camera_idx: int,
-    frame: int
-    ) -> np.ndarray:
-    return 
-
-
-def distance_all_joints_to_central_joint(
-    joints_per_camera: List[np.ndarray],
-    central_joint: int,
-    frame: int,
-    joint: int
-    ) -> np.ndarray:
-    '''
-    Return the distance between the joints seen by each camera and the central joint
-    '''
-    
-    number_of_cameras = len(joints_per_camera)
-    dists = []
-    
-    for c in range(number_of_cameras):
-        dist = np.linalg.norm(joints_per_camera[c][frame][joint] - joints_per_camera[central_joint][frame][joint])
-        dists.append(dist)
-        
-    return dists
-
-def placeholder():
-    ''' Computes equation (5) in an efficient way, for all cameras and joints '''
-    return np.linalg.norm(reshaped_skeletons - theta_i, axis=3)
+    viewer = MoCapViewer(grid_axis=None)
+    # plotting the fused version
+    viewer.add_point_cloud_animation(pcds)
+    viewer.add_skeleton(skeleton_dfs[0].iloc[start_f:end_f], color='green')
+    viewer.add_skeleton(skeleton_dfs[1].iloc[start_f:end_f], color='green')
+    viewer.add_skeleton(skeleton_dfs[2].iloc[start_f:end_f], color='green')
+    viewer.add_skeleton(fused_df.iloc[start_f:end_f], color='red')
+    viewer.show_window()
