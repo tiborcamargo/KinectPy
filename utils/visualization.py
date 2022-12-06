d55b209b658954b26faf8249d5da972f7ef194a7
Tibor <tibor.zequini@gmail.com>
2022-12-06 15:37:03 +0100

    added visualization of skeleton timeseries

--- utils/visualization.py
+++ utils/visualization.py
@@ -85,4 +85,139 @@ def display_pcd_and_joints(
                 color=c
                 )
 
+    # sub1 = 0022..., sub2 = 0017... 
+    print('Red: master\tGreen: sub 1\tBlue: sub 2')
     viewer.show_window()
+
+def visualize_skeletons_2d(
+        skeleton_dataframes: List[pd.DataFrame], 
+        start_frame: int = 0, 
+        end_frame: int = 10000, 
+        skip_every_n_frame: int = 5,
+        plane: str = 'xy',
+        title: str = '',
+        include_frame: bool = False,
+        xlim: List[int] = [-950, 1050],
+        ylim: List[int] = [700, -1100],
+        labels: List[str] = ''
+    ) -> None:
+    ''' 
+    Plotting n skeletons in the 2d selected `plane` 
+    where n = len(skeleton_dataframes)
+    '''
+    import matplotlib.pyplot as plt
+
+    if plane == 'xy':
+        coords = [0, 1]
+    if plane == 'xz':
+        coords = [0, 2]
+    if plane == 'yz':
+        coords = [1, 2]
+
+    if labels != '':
+        assert len(labels) == len(skeleton_dataframes), 'The list of labels must match the number of skeletons'
+
+    end_frame = min(end_frame, len(skeleton_dataframes[0]))
+    for frame in np.arange(start_frame, end_frame, skip_every_n_frame):
+        plt.figure(figsize=(8, 8))
+        plt.grid()
+        plt.xlim(xlim)
+        plt.ylim(ylim)
+        plt.xlabel('x-axis (mm)')
+        plt.ylabel('y-axis (mm)')
+        for device_idx in range(len(skeleton_dataframes)):
+            alpha = 0.4
+            if device_idx == 0:
+                c = 'r'
+            if device_idx == 1:
+                c = 'b'
+            if device_idx == 2:
+                c = 'y'
+            if device_idx > 2:
+                c = 'k'
+                alpha = 1
+            
+            ith_frame = skeleton_dataframes[device_idx].iloc[frame].values.reshape(-1, 3)
+            
+            x1 = ith_frame[:7, coords[0]]
+            x2 = np.concatenate((ith_frame[:3, coords[0]], ith_frame[7:, coords[0]]))
+            y1 = ith_frame[:7, coords[1]]
+            y2 = np.concatenate((ith_frame[:3, coords[1]], ith_frame[7:, coords[1]]))
+
+            if labels == '':
+                label = 'Sensor Idx: ' + str(device_idx)
+            else:
+                label = labels[device_idx]
+
+            plt.plot(x1, y1, marker='o', label=label, c=c, alpha=alpha)  
+            plt.plot(x2, y2, marker='o', c=c, alpha=alpha)  
+
+        fig_title = f'{title}'
+        if include_frame:
+            fig_title += f' - Frame: {frame}'
+        plt.title(fig_title)
+        plt.legend()
+    plt.show()
+
+
+def generate_timeseries_plot(
+        root_dir: str = 'E:/Extracted_data/p1/normal/il_cmj/',
+        joint: str = 'PELVIS',
+        alpha: float = 0.5,
+        beta: float = 0.5
+    ) -> None:
+    '''
+    Visualize a time-series of each x,y, z axis for a certain joint
+    '''
+    import os
+    import matplotlib.pyplot as plt
+    from utils.skeleton_fusion import fuse_skeletons_gradient_centroid
+
+    root_dir = os.path.abspath(root_dir)
+    patient = root_dir.split('\\')[2]
+
+    master_1_df = pd.read_csv(os.path.join(root_dir, 'master_1/skeleton/registered_and_synced_positions_3d.csv'),
+                              delimiter=';')
+    sub_1_df = pd.read_csv(os.path.join(root_dir, 'sub_1/skeleton/registered_and_synced_positions_3d.csv'),
+                           delimiter=';')
+    sub_2_df = pd.read_csv(os.path.join(root_dir, 'sub_2/skeleton/registered_and_synced_positions_3d.csv'),
+                           delimiter=';')
+
+    min_len = min(len(master_1_df), len(sub_1_df), len(sub_2_df))
+
+    master_1_df = master_1_df[[joint + ' (x)', joint + ' (y)', joint + ' (z)']].iloc[:min_len]
+    sub_1_df = sub_1_df[[joint + ' (x)', joint + ' (y)', joint + ' (z)']].iloc[:min_len]
+    sub_2_df = sub_2_df[[joint + ' (x)', joint + ' (y)', joint + ' (z)']].iloc[:min_len]
+
+    skeleton_dfs = [master_1_df, sub_1_df, sub_2_df]
+    skeletons = np.array(
+        [skeleton_dfs[i].values.reshape(skeleton_dfs[i].shape[0], -1, 3) for i in range(len(skeleton_dfs))])
+    fused_skeleton = fuse_skeletons_gradient_centroid(skeletons, alpha=alpha, beta=beta)[:, 0, :]
+
+    plt.rcParams.update({'font.size': 14})
+    fig, ax = plt.subplots(3, 1, sharex=True, squeeze=True)
+    fig.set_size_inches(25, 12)
+
+    ax[0].set_ylabel('x position (mm)');  # ax[0].set_xlabel('frame');
+    ax[0].plot(master_1_df[joint + ' (x)'].values, c='g')
+    ax[0].plot(sub_1_df[joint + ' (x)'].values, c='b')
+    ax[0].plot(sub_2_df[joint + ' (x)'].values, c='k')
+    ax[0].plot(fused_skeleton[:, 0], c='r')
+
+    ax[1].set_ylabel('y position (mm)');  # ax[1].set_xlabel('frame');
+    ax[1].plot(master_1_df[joint + ' (y)'].values, c='g', label='$C_1$')
+    ax[1].plot(sub_1_df[joint + ' (y)'].values, c='b', label='$C_2$')
+    ax[1].plot(sub_2_df[joint + ' (y)'].values, c='k', label='$C_3$')
+    ax[1].plot(fused_skeleton[:, 1], c='r', label=r'Fusion $\alpha = {}, \beta = {}$'.format(alpha, beta))
+    ax[1].legend()
+
+    ax[2].set_ylabel('z position (mm)');
+    ax[2].set_xlabel('frame');
+    ax[2].plot(master_1_df[joint + ' (z)'].values, c='g')
+    ax[2].plot(sub_1_df[joint + ' (z)'].values, c='b')
+    ax[2].plot(sub_2_df[joint + ' (z)'].values, c='k')
+    ax[2].plot(fused_skeleton[:, 2], c='r')
+
+    fig.suptitle(f'Patient: {patient.upper()} - Joint: {joint}')
+    fig.set_tight_layout(tight=True)
+    plt.show()
